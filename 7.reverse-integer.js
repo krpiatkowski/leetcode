bba1dc6d120c44e878a9cafd671471f30b808187
Krzysztof Piatkowski <kpi@trifork.com>
2019-02-24 15:39:04 +0100

    Implemented reverse integer

---
+++ 7.reverse-integer.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=7 lang=javascript
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (25.10%)
+ * Total Accepted:    607.1K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '123'
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ *
+ * Example 1:
+ *
+ *
+ * Input: 123
+ * Output: 321
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: -123
+ * Output: -321
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: 120
+ * Output: 21
+ *
+ *
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of
+ * this problem, assume that your function returns 0 when the reversed integer
+ * overflows.
+ *
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let isNegative = x < 0;
+
+  let absX = Math.abs(x);
+  let reversed = absX
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  let result = isNegative ? "-" + reversed : reversed;
+
+  let r = parseInt(result);
+  if (-2147483648 < r && r < 2147483647) {
+    return result;
+  } else {
+    return "0";
+  }
+};
+
+if (process.env.local) {
+  let input = 1534236469;
+  console.log(JSON.stringify(reverse(input)));
+}
